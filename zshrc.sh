a786013bc560fcc915f4258998ded01519adefb9
Brook Nash <158288359+brookdotcn@users.noreply.github.com>
2025-01-13 12:45:51 +0000

    feat (zshrc): change count

--- zshrc.sh
+++ zshrc.sh
@@ -3,8 +3,14 @@ separator="::"
 current_directory="%F{blue}%1d%f"
 
 set_prompt() {
+  # Read the short, abbreviated name of the current git 'object'
+  # https://git-scm.com/docs/git-rev-parse#Documentation/git-rev-parse.txt---abbrev-refstrictloose
   git_branch=$(if [ -d ".git" ]; then echo "$separator%F{magenta}$(git rev-parse --abbrev-ref HEAD)%f"; fi)
-  PROMPT="$hour_min_sec $current_directory$git_branch "
+
+  # Read the file names of all files in the diff, count the words, trim the whitespace
+  git_change_count=$(if [ -d ".git" ]; then echo "(%F{red}$(git diff --name-only | wc -w | tr -d ' ')%f)"; fi)
+
+  PROMPT="$hour_min_sec $current_directory$git_branch$git_change_count "
 }
 
 setopt PROMPT_SUBST
@@ -15,5 +21,9 @@ set_prompt
 autoload -U add-zsh-hook
 add-zsh-hook chpwd set_prompt
 
-source ~/.cargo/env
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Command history search
+bindkey "^[f" history-beginning-search-forward # opt+left
+bindkey "^[b" history-beginning-search-backward # opt+right
+
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
